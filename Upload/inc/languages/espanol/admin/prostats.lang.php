646afe4ea60dba18164d7d8fdf92789d5e97834a
Vintagedaddyo <vintagedaddyo@gmail.com>
2019-03-09 18:34:51 -0800

    further localization prep

---
+++ Upload/inc/languages/espanol/admin/prostats.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ProStats Plugin - English language
+ */
+
+// Plugin info
+
+$l['pstats_PName'] = '<img border="0" src="../images/prostats/prostats.png" align="absbottom" /> ProStats <span style="color:#000;">/proʊˈstæts/</span>';
+$l['pstats_PDesc'] = 'Professional stats for MyBB. ';
+$l['pstats_PWeb'] = 'http://prostats.wordpress.com';
+$l['pstats_PAuth'] = '<a href="mailto:SaeedGhMail@Gmail.com">SaeedGh</a> and <a href="http://my-bb.ir">AliReza Tofighi</a> updated by <a href="http://community.mybb.com/user-6029.html">vintagedadyo</a>';
+$l['pstats_PAuthSite'] = '';
+$l['pstats_PVer'] = '1.9.7.1';
+$l['pstats_PGUID'] = '124b68d05dcdaf6b7971050baddf340f';
+$l['pstats_PCompat'] = '18*';
+
+
+$l['pstats_settings_link'] = '(<a href="index.php?module=config&action=change&search=prostats" style="color:#FF1493;">Settings</a>)<br/>';
+
+?>
